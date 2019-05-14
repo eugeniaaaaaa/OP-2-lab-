00fbe053ed07c184680b2bfdc149ea72afb6cbaf
jennybamby <45148607+jennybamby@users.noreply.github.com>
2019-05-15 02:05:28 +0300

    Программа

---
+++ lab_2.cpp
@@ -0,0 +1,88 @@
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+void check_open(ifstream &f)
+{
+	if (!f)
+	{
+		cout << "Файл не открыт. ";
+		system("pause");
+		exit(1);
+	}
+}
+
+
+int main()
+{
+	ifstream file("premier_league1.csv");
+	string str, num;
+	getline(file, str);
+	int n = stoi(str);
+	int *result = new int[n];
+	int i = 0;
+	while (file)
+	{
+		result[i] = 0;
+		getline(file, str);
+		str.erase(0, str.find(';') + 1);
+		int arr[20], j = 0;
+		for (int i = 0; i < str.size(); i++)
+		{
+			if (isdigit(str[i]))
+			{
+				num = str[i];
+				arr[j] = stoi(num);
+				j++;
+			}
+		}
+		for (int k = 0; k < 20 - 1; k = k + 2)
+		{
+			if (arr[k] > arr[k + 1])
+			{
+				result[i] = result[i] + 3;
+			}
+			if (arr[k] == arr[k + 1])
+			{
+				result[i] = result[i] + 1;
+			}
+		}
+		i++;
+	}
+	file.close();
+	ifstream file2("premier_league1.csv");
+	ofstream file1("result.csv");
+	getline(file2, str);
+
+	string name, winner;
+	int best = result[0];
+	getline(file2, str);
+	str.erase(str.find(';'), str.find('\0'));
+	file1 << str << ";" << result[0] << endl;
+	winner = str;
+	i = 1;
+	while (file2)
+	{
+		if (i < 20)
+		{
+			getline(file2, str);
+			str.erase(str.find(';'), str.find('\0'));//оставляем название команды
+			file1 << str << ";" << result[i] << endl;
+			if (result[i] > best)//если результат предыдущего меньше результата текущего
+			{
+				best = result[i];
+				winner = str;//присваиваем победителю название команды
+			}
+			i++;
+		}
+		else break;
+	}
+
+	file1 << endl << winner << endl;
+	file1.close();
+
+}
